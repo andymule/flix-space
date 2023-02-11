7a042bb0e4df6bc7ae92f9edd8eaf71157f5387e
Andy Muehlhausen <mule@apple.com>
2023-02-11 10:39:27 -0800

    swifter

--- Package.swift
+++ Package.swift
@@ -3,21 +3,19 @@
 
 import PackageDescription
 
-let package = Package(
+let package: Package = Package(
     name: "SwiftGame",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AdamEisfeld/PhyKit.git", from: "1.0.1"),
-        .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master")
+        .package(url: "https://github.com/STREGAsGate/Raylib.git", branch: "master"),
+
+        // change branch to master after PR goes thru
+        .package(url: "https://github.com/SwifterSwift/SwifterSwift.git", branch: "HKActivitySummary-macos")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "SwiftGame",
-            dependencies: ["PhyKit", "Raylib"] //,
-            // resources: [.copy("Resources")]
+            dependencies: ["PhyKit", "Raylib", "SwifterSwift"]
             ),
     ]
 )
